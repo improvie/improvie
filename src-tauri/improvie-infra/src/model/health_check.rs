286b50c9b38cbee9daddddd61bcc9cc699edc05b
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-01-26 08:37:52 +0900

    feat: add more_convert

--- src-tauri/improvie-infra/src/model/health_check.rs
+++ src-tauri/improvie-infra/src/model/health_check.rs
@@ -1,11 +1,11 @@
 use chrono::{DateTime, Local};
 use improvie_domain::model::health_check::SettingsModel;
 use improvie_logic::Uuid;
+use more_convert::Convert;
 use sqlx::prelude::FromRow;
-use struct_convert::Convert;
 
 #[derive(FromRow, Convert)]
-#[convert(into = "SettingsModel")]
+#[convert(into(SettingsModel))]
 pub struct SettingsRow {
     pub id: Uuid,
     pub created_at: DateTime<Local>,

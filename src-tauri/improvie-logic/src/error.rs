f6c08163dc5cadb2c84a6b7bb650ec8992ac67d3
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-02-16 16:29:37 +0900

    feat: use EnumName on error to kind

--- src-tauri/improvie-logic/src/error.rs
+++ src-tauri/improvie-logic/src/error.rs
@@ -1,10 +1,9 @@
-use serde::{Serialize, Serializer};
+use more_convert::EnumName;
+use serde::{ser::SerializeStruct, Serializer};
 
 pub type AppResult<T> = std::result::Result<T, AppError>;
 
-#[derive(Debug, thiserror::Error, Serialize)]
-#[serde(tag = "kind", content = "message")]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug, thiserror::Error, more_convert::EnumName)]
 pub enum AppError {
     #[error("{1} already exist on {0}")]
     AlreadyExist(&'static str, String),
@@ -16,19 +15,20 @@ pub enum AppError {
     Invalid(&'static str, String),
 
     #[error("db error: {0}")]
-    #[serde(serialize_with = "to_string")]
     Db(#[from] sqlx::Error),
 
     #[error("io error: {0}")]
-    #[serde(serialize_with = "to_string")]
     Io(#[from] std::io::Error),
 }
 
-#[inline]
-fn to_string<S, T>(t: &T, s: S) -> Result<S::Ok, S::Error>
-where
-    S: Serializer,
-    T: ToString,
-{
-    s.serialize_str(t.to_string().as_ref())
+impl serde::Serialize for AppError {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut serde_state = serializer.serialize_struct("AppError", 2)?;
+        serde_state.serialize_field("kind", self.enum_name())?;
+        serde_state.serialize_field("message", &self.to_string())?;
+        serde_state.end()
+    }
 }

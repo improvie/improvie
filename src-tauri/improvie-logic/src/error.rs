17f4113ca43cf5f76d15831b1c63c23061dd9de8
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-02-25 16:40:37 +0900

    fix: remove unneccesary error

--- src-tauri/improvie-logic/src/error.rs
+++ src-tauri/improvie-logic/src/error.rs
@@ -4,20 +4,8 @@ pub type AppResult<T> = std::result::Result<T, AppError>;
 
 #[derive(Debug, thiserror::Error, more_convert::EnumName)]
 pub enum AppError {
-    #[error("{1} already exist on {0}")]
-    AlreadyExist(&'static str, String),
-
-    #[error("{1} not found in {0}")]
-    NotFound(&'static str, String),
-
-    #[error("invalid {1} in {0}")]
-    Invalid(&'static str, String),
-
     #[error("db error: {0}")]
     Db(#[from] sqlx::Error),
-
-    #[error("io error: {0}")]
-    Io(#[from] std::io::Error),
 }
 
 crate::impl_serialize_for_dyn_app_error!(AppError);

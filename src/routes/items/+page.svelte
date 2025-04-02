83ceef9c8aa055760087e115f1bc8be8aabb1d11
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-04-02 23:48:04 +0900

    feat: support many items and play_items

--- src/routes/items/+page.svelte
+++ src/routes/items/+page.svelte
@@ -1,6 +1,7 @@
 <script lang='ts'>
   import { Button } from '$lib/components/ui/button';
   import * as ContextMenu from '$lib/components/ui/context-menu/index.js';
+  import { ScrollArea } from '$lib/components/ui/scroll-area/index.js';
   import Separator from '$lib/components/ui/separator/separator.svelte';
   import * as Table from '$lib/components/ui/table/index.js';
   import CreateContentDialog from '$lib/features/dialog/items/CreateContentDialog.svelte';
@@ -55,38 +56,40 @@
 <Separator class='my-2' />
 <ItemPageBreadcrumb />
 <Separator class='my-2' />
-<Table.Root class='table-fixed select-none'>
-  <Table.Header>
-    <Table.Row>
-      <Table.Head class='w-8'>Name</Table.Head>
-      <Table.Head></Table.Head>
-      <!-- <Table.Head>Description</Table.Head> -->
-      <Table.Head class='text-right'>CreatedAt</Table.Head>
-    </Table.Row>
-  </Table.Header>
-  <Table.Body>
-    {#each node as child}
-      {#if child.kind === 'Folder'}
-        <HierarchyFolder folder_id={child.id} bind:rename_data />
-      {:else if child.kind === 'Content'}
-        <HierarchyContent content_id={child.id} bind:rename_data />
-      {/if}
-    {/each}
-  </Table.Body>
-</Table.Root>
-<ContextMenu.Root>
-  <ContextMenu.Trigger class='w-full h-[-webkit-fill-available]'>
-  </ContextMenu.Trigger>
-  <ContextMenu.Content>
-    <ContextMenu.Item onclick={() => {
-      is_open_create_content = true;
-    }} class='flex items-center'>
-      <ImportIcon class='mr-2 size-4' />Add Item
-    </ContextMenu.Item>
-    <ContextMenu.Item onclick={() => {
-      is_open_create_folder = true;
-    }} class='flex items-center'>
-      <FolderIcon class='mr-2 size-4' />Add Folder
-    </ContextMenu.Item>
-  </ContextMenu.Content>
-</ContextMenu.Root>
+<ScrollArea class='w-full h-dvh'>
+  <Table.Root class='table-fixed select-none'>
+    <Table.Header>
+      <Table.Row>
+        <Table.Head class='w-8'>Name</Table.Head>
+        <Table.Head></Table.Head>
+        <!-- <Table.Head>Description</Table.Head> -->
+        <Table.Head class='text-right'>CreatedAt</Table.Head>
+      </Table.Row>
+    </Table.Header>
+    <Table.Body>
+      {#each node as child}
+        {#if child.kind === 'Folder'}
+          <HierarchyFolder folder_id={child.id} bind:rename_data />
+        {:else if child.kind === 'Content'}
+          <HierarchyContent content_id={child.id} bind:rename_data />
+        {/if}
+      {/each}
+    </Table.Body>
+  </Table.Root>
+  <ContextMenu.Root>
+    <ContextMenu.Trigger class='w-full min-h-72 h-fit'>
+    </ContextMenu.Trigger>
+    <ContextMenu.Content>
+      <ContextMenu.Item onclick={() => {
+        is_open_create_content = true;
+      }} class='flex items-center'>
+        <ImportIcon class='mr-2 size-4' />Add Item
+      </ContextMenu.Item>
+      <ContextMenu.Item onclick={() => {
+        is_open_create_folder = true;
+      }} class='flex items-center'>
+        <FolderIcon class='mr-2 size-4' />Add Folder
+      </ContextMenu.Item>
+    </ContextMenu.Content>
+  </ContextMenu.Root>
+</ScrollArea>

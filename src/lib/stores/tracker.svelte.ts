7c5a4e503fce1ee46e0d3f8c75cdb30212affb50
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-04-26 00:15:13 +0900

    fix: remove version

--- src/lib/stores/tracker.svelte.ts
+++ src/lib/stores/tracker.svelte.ts
@@ -15,12 +15,6 @@ export class Tracker {
   public is_looping: boolean = $state()!;
   public volume: number = $state()!;
 
-  private version: number = $state(0);
-
-  get track_version(): number {
-    return this.version;
-  }
-
   public init() {
     this.is_looping
       = getLocalStorageOrDefault('is_looping', 'false') === 'true';
@@ -35,17 +29,9 @@ export class Tracker {
     });
   }
 
-  private update_version() {
-    if (this.version > 1000) {
-      this.version = 0;
-    }
-    this.version++;
-  }
-
   public set_single_content(id: string) {
     const prev_track_id = this.current_track_id;
     this.clear_track();
-    this.update_version();
 
     this.current_track_id = id;
     if (prev_track_id === id) {
@@ -71,7 +57,6 @@ export class Tracker {
   public reset_track() {
     const id = this.current_track_id;
     this.clear_track();
-    this.update_version();
     this.current_track_id = id;
     this.paused = false;
   }
@@ -99,7 +84,6 @@ export class Tracker {
 
     this.play_rules = rules;
     if (rules.length > 0) {
-      this.update_version();
       this.update_current_track();
     }
     else {
@@ -117,7 +101,6 @@ export class Tracker {
       const prev_track_id = this.current_track_id;
       this.currentTime = 0;
       this.paused = false;
-      this.update_version();
       this.current_track_id = this.play_rules[this.current_rule_idx].content_id;
       if (prev_track_id === this.current_track_id) {
         this.paused = false;

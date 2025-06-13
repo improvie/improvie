8b8b0cc18c0e5c43cd7d6337c9a858dea204abb0
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-06-13 21:40:21 +0900

    refactor: update ez-ffmpeg and fast process

--- src-tauri/addtional/youtube/src/ffmpeg.rs
+++ src-tauri/addtional/youtube/src/ffmpeg.rs
@@ -13,52 +13,6 @@ macro_rules! try_ffmpeg {
 }
 
 #[cfg(not(feature = "include"))]
-pub(crate) async fn movflags_faststart(_from: &Path, _to: &Path) -> Result<(), YtError> {
-    unimplemented!("movflags_faststart is not supported without the `include` feature");
-}
-
-#[cfg(feature = "include")]
-pub(crate) async fn movflags_faststart(from: &Path, to: &Path) -> Result<(), YtError> {
-    log::info!("Processing movflags faststart");
-    let build = ez_ffmpeg::FfmpegContext::builder()
-        .input(from.to_string_lossy().to_string())
-        .output(
-            ez_ffmpeg::Output::new(to.to_string_lossy().to_string())
-                .add_stream_map_with_copy("0:v?")
-                .add_stream_map_with_copy("0:a?")
-                .set_format_opt("movflags", "faststart"),
-        )
-        .build();
-    let start = try_ffmpeg!(build).start();
-    let result = try_ffmpeg!(start).await;
-    try_ffmpeg!(result);
-    log::info!("Finished processing movflags faststart");
-    Ok(())
-}
-
-#[cfg(not(feature = "include"))]
-pub(crate) async fn audio_to_acc(_from: &Path, _to: &Path) -> Result<(), YtError> {
-    unimplemented!("audio_to_acc is not supported without the `include` feature");
-}
-
-#[cfg(feature = "include")]
-pub(crate) async fn audio_to_acc(from: &Path, to: &Path) -> Result<(), YtError> {
-    log::info!("Converting audio to acc");
-    let build = ez_ffmpeg::FfmpegContext::builder()
-        .input(from.to_string_lossy().to_string())
-        .output(
-            ez_ffmpeg::Output::new(to.to_string_lossy().to_string())
-                .add_stream_map("0:a")
-                .set_audio_codec("aac"),
-        )
-        .build();
-    let start = try_ffmpeg!(build).start();
-    let result = try_ffmpeg!(start).await;
-    try_ffmpeg!(result);
-    Ok(())
-}
-
-#[cfg(not(feature = "include"))]
 pub(crate) async fn merge_video_audio(
     _video_path: &Path,
     _audio_path: &Path,
@@ -80,7 +34,9 @@ pub(crate) async fn merge_video_audio(
         .output(
             ez_ffmpeg::Output::new(output_path.to_string_lossy().to_string())
                 .add_stream_map_with_copy("0:v?")
-                .add_stream_map_with_copy("1:a?"),
+                .add_stream_map_with_copy("1:a?")
+                .set_video_codec_opt("preset", "fast")
+                .set_format_opt("movflags", "faststart"),
         )
         .build();
     let start = try_ffmpeg!(build).start();

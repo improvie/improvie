ecf107c0c7c46271c1bc794cd842b9934f64e04e
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-06-18 20:19:58 +0900

    feat: add YtVideoComponent

--- src/lib/features/youtube/YtVideoComponent.svelte
+++ src/lib/features/youtube/YtVideoComponent.svelte
@@ -1,11 +1,14 @@
 <script lang='ts'>
-  import type { VideoDetail } from '$lib/youtube';
+  import type { YtVideoDownloadComplete, YtVideoState } from '$bindings/yt';
   import ImageLoader from '$lib/components/ImageLoader.svelte';
   import { Button } from '$lib/components/ui/button/index.js';
   import * as Card from '$lib/components/ui/card/index.js';
   import { Label } from '$lib/components/ui/label/index.js';
   import * as RadioGroup from '$lib/components/ui/radio-group/index.js';
   import { ScrollArea } from '$lib/components/ui/scroll-area/index.js';
+  import { create_content } from '$lib/stores/items/content';
+  import { import_youtube_video, type VideoDetail } from '$lib/youtube';
+  import { listen } from '@tauri-apps/api/event';
 
   const {
     parent_folder_id,
@@ -16,6 +19,64 @@
   } = $props();
 
   let downloadUrl = $state<string>(detail.video_formats[0].url);
+
+  let process = $state<YtVideoState | undefined>(undefined);
+
+  const unlisten = listen<YtVideoState>('yt-downloading-state', (event) => {
+    const payload = event.payload;
+    switch (payload.type) {
+      case 'Idle':
+        if (payload.data.video_id !== detail.video_id) {
+          return;
+        }
+        process = payload;
+        break;
+      case 'Downloading':
+        if (payload.data.video_id !== detail.video_id) {
+          return;
+        }
+        process = payload;
+        break;
+      case 'Completed':
+        if (payload.data.video_id !== detail.video_id) {
+          return;
+        }
+        process = payload;
+        saveContent(payload.data.state);
+        break;
+    }
+  });
+
+  async function saveContent(request: YtVideoDownloadComplete) {
+    await create_content({
+      kind: 'Video',
+      content_path: request.video_path,
+      thumbnail_path: request.thumbnail_path ?? null,
+      parent_folder_id,
+      title: detail.title,
+      description: null,
+    });
+    console.log('Video saved:', request.video_path);
+  }
+
+  $effect(() => {
+    return () => {
+      unlisten.then((fn) => {
+        fn();
+      });
+      process = undefined;
+    };
+  });
+
+  function importVideo() {
+    import_youtube_video({
+      video_id: detail.video_id,
+      content_title: detail.title,
+      video_url: downloadUrl,
+      audio_url: detail.best_audio.url,
+      thumbnail_url: detail.thumbnail_url ?? null,
+    });
+  }
 
 </script>
 
@@ -45,7 +106,7 @@
     </ScrollArea>
     <Button
       onclick={() => {
-
+        importVideo();
       }}
     >
       Import Video

b17db44bb6ff73550356868266d8f68409453394
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-06-20 12:04:57 +0900

    feat: logable multiple args

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,7 +1,8 @@
-import { debug, error, info, trace, warn } from '@tauri-apps/plugin-log';
-
 // Tauri doesn't have a Node.js server to do proper SSR
 // so we will use adapter-static to prerender the app (SSG)
+
+import { debug, error, info, trace, warn } from '@tauri-apps/plugin-log';
+
 // See: https://v2.tauri.app/start/frontend/sveltekit/ for more info
 export const prerender = true;
 export const ssr = false;
@@ -9,16 +10,13 @@ export const ssr = false;
 /* eslint-disable no-console */
 function forwardConsole(
   fnName: 'log' | 'debug' | 'info' | 'warn' | 'error',
-  logger: (message: string) => Promise<void>,
+  logger: (...data: any[]) => void,
 ) {
   const original = console[fnName];
   console[fnName] = (...data: any[]) => {
     original(...data);
     const message = data.map(d => (typeof d === 'object' ? JSON.stringify(d) : d)).join(' ');
-    logger(message).catch((err) => {
-      // If the logger fails, we log to the console as a fallback
-      console.error(`Failed to log message with ${fnName}:`, err);
-    });
+    logger(message);
   };
 }
 

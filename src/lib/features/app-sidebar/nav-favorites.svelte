d72f7cf80f31476dcf31298b444ff5ae492cb691
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-07-15 18:17:32 +0900

    feat: add favorite playlist menu item on sidebar

--- src/lib/features/app-sidebar/nav-favorites.svelte
+++ src/lib/features/app-sidebar/nav-favorites.svelte
@@ -1,10 +1,14 @@
 <script lang='ts'>
+  import { goto } from '$app/navigation';
+  import { page } from '$app/state';
   import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
   import * as Sidebar from '$lib/components/ui/sidebar/index.js';
   import { useSidebar } from '$lib/components/ui/sidebar/index.js';
   import { select_playlist } from '$lib/stores/plays';
   import { favoritePlaylists, initFavoritePlaylist, removeFavoritePlaylist } from '$lib/stores/plays/favorite';
   import { playlists } from '$lib/stores/plays/playlist';
+  import { StarIcon } from '@lucide/svelte';
+
   import Ellipsis from '@lucide/svelte/icons/ellipsis';
   import StarOff from '@lucide/svelte/icons/star-off';
   import { onMount } from 'svelte';
@@ -22,46 +26,62 @@
   });
 
   const sidebar = useSidebar();
+
+  const favoritePlaylistPath = '/favorite/playlists';
 </script>
 
-<Sidebar.Group class='group-data-[collapsible=icon]:hidden'>
-  <Sidebar.GroupLabel>Favorite Playlists</Sidebar.GroupLabel>
-  <Sidebar.Menu>
-    {#each favorites as playlist}
+<Sidebar.Group>
+  <Sidebar.GroupContent>
+    <Sidebar.Menu>
       <Sidebar.MenuItem>
-        <Sidebar.MenuButton onclick={() => select_playlist(playlist.id)}>
-          <span>{playlist.title}</span>
+        <Sidebar.MenuButton
+          isActive={page.url.pathname === favoritePlaylistPath}
+          onclick={() => {
+            goto(favoritePlaylistPath);
+          }}
+        >
+          <StarIcon />
+          <span class='line-clamp-1 text-wrap'>Favorite Playlists</span>
         </Sidebar.MenuButton>
-        <DropdownMenu.Root>
-          <DropdownMenu.Trigger>
-            {#snippet child({ props })}
-              <Sidebar.MenuAction {...props}>
-                <Ellipsis />
-              </Sidebar.MenuAction>
-            {/snippet}
-          </DropdownMenu.Trigger>
-          <DropdownMenu.Content
-            class='w-56 rounded-lg'
-            side={sidebar.isMobile ? 'bottom' : 'right'}
-            align={sidebar.isMobile ? 'end' : 'start'}
-          >
-            <DropdownMenu.Item
-              onclick={async () => {
-                await removeFavoritePlaylist(playlist.id);
-              }}
-            >
-              <StarOff class='text-muted-foreground' />
-              <span>Remove</span>
-            </DropdownMenu.Item>
-          </DropdownMenu.Content>
-        </DropdownMenu.Root>
+        <Sidebar.MenuSub>
+          {#each favorites as playlist}
+            <Sidebar.MenuSubItem>
+              <Sidebar.MenuSubButton onclick={() => select_playlist(playlist.id)}>
+                <span>{playlist.title}</span>
+              </Sidebar.MenuSubButton>
+              <DropdownMenu.Root>
+                <DropdownMenu.Trigger>
+                  {#snippet child({ props })}
+                    <Sidebar.MenuAction {...props}>
+                      <Ellipsis />
+                    </Sidebar.MenuAction>
+                  {/snippet}
+                </DropdownMenu.Trigger>
+                <DropdownMenu.Content
+                  class='w-56 rounded-lg'
+                  side={sidebar.isMobile ? 'bottom' : 'right'}
+                  align={sidebar.isMobile ? 'end' : 'start'}
+                >
+                  <DropdownMenu.Item
+                    onclick={async () => {
+                      await removeFavoritePlaylist(playlist.id);
+                    }}
+                  >
+                    <StarOff class='text-muted-foreground' />
+                    <span>Remove</span>
+                  </DropdownMenu.Item>
+                </DropdownMenu.Content>
+              </DropdownMenu.Root>
+            </Sidebar.MenuSubItem>
+          {:else}
+            <Sidebar.MenuSubItem>
+              <Sidebar.MenuSubButton class='text-sidebar-foreground/70' aria-disabled>
+                <span>Empty</span>
+              </Sidebar.MenuSubButton>
+            </Sidebar.MenuSubItem>
+          {/each}
+        </Sidebar.MenuSub>
       </Sidebar.MenuItem>
-    {:else}
-      <Sidebar.MenuItem>
-        <Sidebar.MenuButton class='text-sidebar-foreground/70' aria-disabled>
-          <span>Empty</span>
-        </Sidebar.MenuButton>
-      </Sidebar.MenuItem>
-    {/each}
-  </Sidebar.Menu>
+    </Sidebar.Menu>
+  </Sidebar.GroupContent>
 </Sidebar.Group>

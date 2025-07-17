a81e2fb219edc8addad1b0567f2c214115f54421
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-07-17 19:28:30 +0900

    wip: same the previous

--- src-tauri/improvie-infra/src/repository/rules.rs
+++ src-tauri/improvie-infra/src/repository/rules.rs
@@ -1,38 +1,35 @@
 use improvie_domain::{model::rules::RuleData, repository::rules::RulesRepository};
 use improvie_logic::DynAppResult;
-use sqlx::types::Json;
+use improvie_row::playlists::JsonRuleDataList;
+use sea_orm::{ColumnTrait, EntityTrait, QueryFilter, QuerySelect};
 
 super::def_repository_impl!(RulesRepositoryImpl);
 
 #[async_trait::async_trait]
 impl RulesRepository for RulesRepositoryImpl {
     async fn get_rules(&self, playlist_id: uid::Uid) -> DynAppResult<Vec<RuleData>> {
-        let row = sqlx::query_scalar::<_, Json<Vec<RuleData>>>(
-            "
-SELECT rules
-FROM playlists
-WHERE item_id = ?
-",
-        )
-        .bind(playlist_id)
-        .fetch_one(&self.db.pool())
-        .await?;
+        let data = improvie_row::playlists::Entity::find()
+            .filter(improvie_row::playlists::Column::ItemId.eq(playlist_id))
+            .select_only()
+            .column(improvie_row::playlists::Column::Rules)
+            .into_tuple::<JsonRuleDataList>()
+            .one(self.db.pool())
+            .await?;
 
-        Ok(row.0)
+        Ok(data.map(|d| d.0).unwrap_or_default())
     }
 
     async fn update_rules(&self, playlist_id: uid::Uid, rules: Vec<RuleData>) -> DynAppResult<()> {
-        sqlx::query(
-            "
-UPDATE playlists
-SET rules = ?
-WHERE item_id = ?
-",
-        )
-        .bind(Json(rules))
-        .bind(playlist_id)
-        .execute(&self.db.pool())
-        .await?;
+        let row = improvie_row::playlists::ActiveModel {
+            item_id: sea_orm::Set(playlist_id),
+            rules: sea_orm::Set(JsonRuleDataList(rules)),
+            ..Default::default()
+        };
+
+        // TODO: update without returning the row
+        improvie_row::playlists::Entity::update(row)
+            .exec(self.db.pool())
+            .await?;
 
         Ok(())
     }
--- src-tauri/improvie-infra/src/repository/settings.rs
+++ src-tauri/improvie-infra/src/repository/settings.rs
@@ -1,8 +1,9 @@
 use improvie_domain::repository::settings::SettingsRepository;
 use improvie_logic::model::settings::AppSettings;
+use sea_orm::ActiveValue::*;
+use sea_orm::ColumnTrait;
+use sea_orm::{EntityTrait, QueryFilter, QuerySelect};
 use uid::Uid;
-
-use crate::model::settings::AppSettingsRow;
 
 use super::def_repository_impl;
 
@@ -10,22 +11,26 @@ def_repository_impl!(SettingsRepositoryImpl);
 
 #[async_trait::async_trait]
 impl SettingsRepository for SettingsRepositoryImpl {
-    async fn get_app_settings(&self) -> improvie_logic::DynAppResult<AppSettings> {
-        let row: AppSettingsRow = sqlx::query_as::<_, AppSettingsRow>(
-            "SELECT id, settings, created_at FROM app_settings WHERE id = ?",
-        )
-        .bind(Uid::nil())
-        .fetch_one(&self.db.pool())
-        .await?;
-
-        Ok(row.settings)
+    async fn get_app_settings(&self) -> improvie_logic::DynAppResult<Option<AppSettings>> {
+        improvie_row::app_settings::Entity::find()
+            .filter(improvie_row::app_settings::Column::Id.eq(Uid::nil()))
+            .select_only()
+            .column(improvie_row::app_settings::Column::Settings)
+            .into_tuple::<AppSettings>()
+            .one(self.db.pool())
+            .await
+            .map_err(Into::into)
     }
 
     async fn set_app_settings(&self, settings: AppSettings) -> improvie_logic::DynAppResult<()> {
-        sqlx::query("UPDATE app_settings SET settings = ? WHERE id = ?")
-            .bind(sqlx::types::Json(settings))
-            .bind(Uid::nil())
-            .execute(&self.db.pool())
+        let row = improvie_row::app_settings::ActiveModel {
+            id: Set(Uid::nil()),
+            settings: Set(settings),
+            ..Default::default()
+        };
+
+        improvie_row::app_settings::Entity::update(row)
+            .exec(self.db.pool())
             .await?;
 
         Ok(())

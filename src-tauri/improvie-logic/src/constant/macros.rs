0345b25376fd396323e60938f59b64feaf2a19af
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-07-17 18:03:06 +0900

    feat: impl Hash on def_constant_enum

--- src-tauri/improvie-logic/src/constant/macros.rs
+++ src-tauri/improvie-logic/src/constant/macros.rs
@@ -7,7 +7,7 @@ macro_rules! def_constant_enum {
         )+
     ) => {
         $(
-            $(#[$attr])* #[derive(Debug, Clone, Copy, PartialEq, PartialOrd, Ord, Eq, serde::Serialize, serde::Deserialize)]
+            $(#[$attr])* #[derive(Debug, Clone, Copy, PartialEq, PartialOrd, Ord, Eq, Hash, serde::Serialize, serde::Deserialize)]
             #[cfg_attr(feature = "db", derive(sea_orm::EnumIter, sea_orm::DeriveActiveEnum))]
             #[cfg_attr(feature = "db", sea_orm(rs_type = "u8", db_type = "TinyUnsigned"))]
             #[cfg_attr(feature = "ts", bind::ts("constants.ts"))]

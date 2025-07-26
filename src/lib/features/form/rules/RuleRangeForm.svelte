f636dbb76665bc5c072a0c452e07450801283981
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-07-26 10:13:17 +0900

    feat: rewrite Forms

--- src/lib/features/form/rules/RuleRangeForm.svelte
+++ src/lib/features/form/rules/RuleRangeForm.svelte
@@ -1,99 +1,38 @@
 <script lang='ts'>
   import type { RuleType } from '$bindings/rule';
-  import type { PickItem } from '$lib/types/item';
-  import { Button } from '$lib/components/ui/button';
-  import * as Form from '$lib/components/ui/form/index.js';
-  import { Input } from '$lib/components/ui/input';
-  import ContentPicker from '$lib/features/combobox/ContentPicker.svelte';
-  import { defaults, superForm } from 'sveltekit-superforms';
-  import { zod } from 'sveltekit-superforms/adapters';
-  import { z } from 'zod';
-  import FormError from '../FormError.svelte';
+  import type { CommonSchemaToDataType } from '../common/CommonFormSchema.svelte';
+  import CommonForm from '../common/CommonForm.svelte';
+  import { createForm, defineSchema } from '../common/CommonFormSchema.svelte';
 
   let { add_rule = $bindable() }: { add_rule: (rule: RuleType) => void } = $props();
 
-  const formSchema = z.object({
-    content_id: z.string().nonempty(),
-    range_start: z.number().int().nonnegative().default('' as unknown as number),
-    range_end: z.number().int().nonnegative().default('' as unknown as number),
+  const schema = defineSchema({
+    content_id: {
+      type: 'content_pick',
+      label: 'Content',
+    },
+    range: {
+      type: 'range',
+      label: 'Range',
+      props: {
+        min: 0,
+        max: 100,
+      },
+    },
   });
+  const form = createForm(schema);
+  type FormData = CommonSchemaToDataType<typeof schema>;
 
-  const form = superForm(defaults(zod(formSchema)), {
-    SPA: true,
-    validators: zod(formSchema),
-    resetForm: false,
-  });
-
-  const { form: formData, enhance, validateForm } = form;
-
-  let open = $state(false);
-  let pick_content = $state<PickItem | undefined>();
-
-  $effect(() => {
-    if (pick_content) {
-      $formData.content_id = pick_content.id;
-    }
-  });
-
-  async function handleSubmit(event: Event) {
-    event.preventDefault();
-    const result = await validateForm();
-    if (!result.valid) {
-      return;
-    }
-
+  async function handleSubmit(data: FormData) {
     add_rule({
       type: 'Range',
       data: {
-        content_id: $formData.content_id,
-        range_start: $formData.range_start,
-        range_end: $formData.range_end,
+        content_id: data.content_id,
+        range_start: data.range[0],
+        range_end: data.range[1],
       },
     });
   }
 </script>
 
-<ContentPicker bind:content={pick_content} bind:open />
-
-<form method='POST' use:enhance onsubmit={handleSubmit}>
-  <Form.Field {form} name='content_id'>
-    <Form.Control>
-      {#snippet children({ props })}
-        <div class='grid grid-cols-7 items-center gap-4'>
-          <Form.Label class='text-right col-span-2'>Content</Form.Label>
-          <Button
-            class='col-span-5 text-wrap'
-            variant='outline'
-            {...props}
-            onclick={() => open = true}
-          >{pick_content?.hierarchy_name || 'Select content'}
-          </Button>
-        </div>
-      {/snippet}
-    </Form.Control>
-  </Form.Field>
-  <Form.Field {form} name='range_start' class='my-4'>
-    <Form.Control>
-      {#snippet children({ props })}
-        <div class='grid grid-cols-7 items-center gap-4'>
-          <Form.Label class='text-right col-span-2'>Range Start</Form.Label>
-          <Input class='col-span-5' bind:value={$formData.range_start} type='number' {...props} />
-        </div>
-      {/snippet}
-    </Form.Control>
-  </Form.Field>
-  <Form.Field {form} name='range_end'>
-    <Form.Control>
-      {#snippet children({ props })}
-        <div class='grid grid-cols-7 items-center gap-4'>
-          <Form.Label class='text-right col-span-2'>Range End</Form.Label>
-          <Input class='col-span-5' bind:value={$formData.range_end} type='number' {...props} />
-        </div>
-      {/snippet}
-    </Form.Control>
-  </Form.Field>
-  <div class='my-4'></div>
-  <FormError {form} />
-  <div class='my-4'></div>
-  <Button type='submit'>Submit</Button>
-</form>
+<CommonForm {form} {schema} handle={handleSubmit} />
